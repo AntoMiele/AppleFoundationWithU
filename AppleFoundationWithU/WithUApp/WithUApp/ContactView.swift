701d0322d38459e1c8e8a2dbdba1a3c3c006619c
AntoMiele <134271846+AntoMiele@users.noreply.github.com>
2025-10-18 12:55:45 +0200

    Add files via upload

---
+++ AppleFoundationWithU/WithUApp/WithUApp/ContactView.swift
@@ -0,0 +1,18 @@
+//
+//  ContactView.swift
+//  Map_iOS
+//
+//  Created by san-12 on 18/10/25.
+//
+
+import SwiftUI
+
+struct ContactView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContactView()
+}
